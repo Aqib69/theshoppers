b4305bcccc2e837b3b9f2a38dd419bf9b0afd287
KhawajaMuhammadZubairLone <126096143+KhawajaMuhammadZubairLone@users.noreply.github.com>
2023-02-24 13:39:23 +0500

    New project

---
+++ storage/framework/views/7c9217888274a47530c29e98713b8e4b9bafb123.php
@@ -0,0 +1,7 @@
+
+<?php $__env->startSection('main-section'); ?>
+<h1 class="text center">
+    home page
+</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laravel\example-app\resources\views/home.blade.php ENDPATH**/ ?>
